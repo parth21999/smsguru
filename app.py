9bccb3a0f1629f288f84037a63f898ef5a703eba
parthagg <parthagg@umich.edu>
2018-08-01 16:17:56 +0530

    convert spaces to tabs

--- app.py
+++ app.py
@@ -14,11 +14,15 @@ from flask import request
 app = Flask(__name__)
 
 
-@app.route('/', methods=[GET, POST])
+@app.route('/', methods=["GET", "POST"])
 def main_route():
-    if request.method == 'POST':
-        return request.form.get['content']
-	return "hello world!!!"
+	if request.method == 'POST':
+		return request.form.get['content']
+	else:
+		return "hello world!!!"
+
+if __name__ == "__main__":
+	app.run()
 
 # @app.route('/messages', methods=[POST])
 # def messages_route():
@@ -27,46 +31,46 @@ def main_route():
 #     return content
 
 
- 
+
 def sendSMS(apikey, numbers, sender, message):
-    data =  urllib.parse.urlencode({'apikey': apikey, 'numbers': numbers,
-        'message' : message, 'sender': sender})
-    data = data.encode('utf-8')
-    request = urllib.request.Request("https://api.textlocal.in/send/?")
-    f = urllib.request.urlopen(request, data)
-    fr = f.read()
-    return(fr)
+	data =  urllib.parse.urlencode({'apikey': apikey, 'numbers': numbers,
+		'message' : message, 'sender': sender})
+	data = data.encode('utf-8')
+	request = urllib.request.Request("https://api.textlocal.in/send/?")
+	f = urllib.request.urlopen(request, data)
+	fr = f.read()
+	return(fr)
 
 def getInboxes(apikey):
-    data =  urllib.parse.urlencode({'apikey': apikey})
-    data = data.encode('utf-8')
-    request = urllib.request.Request("https://api.textlocal.in/get_inboxes/?")
-    f = urllib.request.urlopen(request, data)
-    fr = f.read()
-    return(fr)
+	data =  urllib.parse.urlencode({'apikey': apikey})
+	data = data.encode('utf-8')
+	request = urllib.request.Request("https://api.textlocal.in/get_inboxes/?")
+	f = urllib.request.urlopen(request, data)
+	fr = f.read()
+	return(fr)
 
 def getMessages(apikey, inboxID):
-    data =  urllib.parse.urlencode({'apikey': apikey, 'inbox_id' : inboxID})
-    data = data.encode('utf-8')
-    request = urllib.request.Request("https://api.textlocal.in/get_messages/?")
-    f = urllib.request.urlopen(request, data)
-    fr = f.read()
-    return(fr)
+	data =  urllib.parse.urlencode({'apikey': apikey, 'inbox_id' : inboxID})
+	data = data.encode('utf-8')
+	request = urllib.request.Request("https://api.textlocal.in/get_messages/?")
+	f = urllib.request.urlopen(request, data)
+	fr = f.read()
+	return(fr)
 
-if __name__ == "__main__":
-    app.run()
-    apikey = 'A4mhT8jM+RY-ePSJWXB0P5pJuT5BzBBlVAumiqQiZJ'
+def stuff():
+	apikey = 'A4mhT8jM+RY-ePSJWXB0P5pJuT5BzBBlVAumiqQiZJ'
 
-    # resp =  sendSMS(apikey, '919205257278',
-    #     'TXTLCL ', 'This is your message')
+	# resp =  sendSMS(apikey, '919205257278',
+	#     'TXTLCL ', 'This is your message')
 
-    # To get ID
-    inboxes_bytes = getInboxes(apikey)
-    inboxes = json.loads(inboxes_bytes.decode('utf8').replace("'", '"'))
-    inbox_id = inboxes["inboxes"][0]["id"]
-    # print (inbox_id)
+	# To get ID
+	inboxes_bytes = getInboxes(apikey)
+	inboxes = json.loads(inboxes_bytes.decode('utf8').replace("'", '"'))
+	inbox_id = inboxes["inboxes"][0]["id"]
+	# print (inbox_id)
 
-    messages_bytes = getMessages(apikey, inbox_id)
-    messages = json.loads(messages_bytes.decode('utf8').replace("'", '"'))
+	messages_bytes = getMessages(apikey, inbox_id)
+	messages = json.loads(messages_bytes.decode('utf8').replace("'", '"'))
 
-    print(messages)
+	print(messages)
+	return
