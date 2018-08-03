c4b2c18e5d969ebf1cf2af376a153c5c9ccb5f15
Arjun Aggarwal <arjun152002@gmail.com>
2018-08-03 17:33:30 +0530

    fixing bug

--- app.py
+++ app.py
@@ -20,7 +20,7 @@ keyword = '7B3D9'
 
 def sendSMS(numbers, sender, message):
 	data =  urllib.parse.urlencode({'username': 'parth21999@gmail.com', 'password': 'Partharjun1', 'numbers': numbers,
-		'message' : message, 'sender': sender, 'unicode' : True})
+		'message' : message, 'sender': sender, 'unicode' : 'True'})
 	data = data.encode('utf-8')
 	request = urllib.request.Request("https://api.textlocal.in/send/?")
 	f = urllib.request.urlopen(request, data)
