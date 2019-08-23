47d4ac832b7be466eb4870332d315810eec2b1ee
Arjun Aggarwal <arjun152002@gmail.com>
2019-08-23 13:12:46 +0530

    added logging

--- App_files/app.py
+++ App_files/app.py
@@ -1,50 +1,27 @@
-#/usr/bin/env python
 # nlp helper functions 
 from App_files.nlp_helpers import (reduce_content, get_keywords, clean_content, 
 check_spellings, summerize_content, clean_sms_content,
 correct_case, detect_language)
-
+# SMS helpers
 from App_files.textlocal_helpers import sendSMS
 # search helpers
 from App_files.search_helpers import (search_duckduckgo, get_google_info, get_wiki_info)
-
+# flask 
 from flask import Flask
 from flask import request
 import os
-
 # miscellanous
 import json
 import wikipedia
 import pickle
-# for database integration
-#import mysql.connector as mysql
-#from flaskext.mysql import MySQL
 from googletrans import Translator
-
+# logging
+import logging
+import datetime
 application = app = Flask(__name__)
-'''
-mysql = MySQL()
-app.config['MYSQL_DATABASE_USER'] = 'sql12300194'
-app.config['MYSQL_DATABASE_PASSWORD'] = 'jkhL9weFyW'
-app.config['MYSQL_DATABASE_DB'] = 'sql12300194'
-app.config['MYSQL_DATABASE_HOST'] = 'sql12.freemysqlhosting.net'
-mysql.init_app(app)
-conn = mysql.connect()
-cursor = conn.cursor()
-'''
 apikey = 'A4mhT8jM+RY-ePSJWXB0P5pJuT5BzBBlVAumiqQiZJ'
 keyword = '7B3D9'
-'''
-def update_database(phoneNumber, query):
-	# Checking if number exists in database
-	print("updating DB")
-	search_results = cursor.execute("SELECT * FROM users WHERE PhoneNumber = (%s)", phoneNumber)
-	print(search_results)
-	if len(search_results) == 0:
-		SQL_formula = "INSERT INTO users (PhoneNumber) VALUES (%s)"
-		cursor.execute(SQL_formula, phoneNumber)
-	conn.commit()
-'''
+
 def get_info(sms_content):
 	cleaned = clean_sms_content(sms_content)
 	#cleaned = check_spellings(cleaned)
@@ -70,15 +47,12 @@ def get_info(sms_content):
 		print(str(e))
 		return "Error occured"
 
-
-
 @app.route('/', methods=["GET", "POST"])
 def main_route():
 	if request.method == "POST":
 		print("query received")
 		sender_number = request.form.get('sender')
 		content = request.form.get('content')
-		#update_database(sender_number, content)
 		credits = request.form.get('credits')
 		info_to_send = get_info(content)
 		print(info_to_send)
@@ -86,9 +60,17 @@ def main_route():
 			print("Sending response")
 			send_resp = sendSMS(sender_number, 'TXTLCL', info_to_send).decode('utf8').replace("'", '"')
 			print("Response: " + send_resp)
+			cur_datetime = datetime.datetime.now().strftime("%Y-%m-%d %H:%M")
+			query_info = f'{cur_datetime},{sender_number},{content},{info_to_send},{send_resp.status}'
+			app.logger.info(query_info)
 
 		# print("MESSAGE CONTENT: " + request.form.get('content'))
 	return "Hello world"
 
 if __name__ == "__main__":
+	handler = logging.FileHandler('usage_logs.csv')
+	handler.setLevel(logging.INFO)
+	f_format = logging.Formatter('%(message)s')
+	handler.setFormatter(f_format)
+	app.logger.addHandler(handler)
 	app.run()
